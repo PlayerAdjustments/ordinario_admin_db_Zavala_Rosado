7a7c3ffb6e16d933b5d80020df02f52525b6a577
PlayerAdjustments <ferzg2004@gmail.com>
2024-12-06 20:42:56 -0600

    Removed unused function in asignaturaController

--- src/controllers/asignaturasController.js
+++ src/controllers/asignaturasController.js
@@ -28,23 +28,4 @@ const createAsignatura = (req, res) => {
     });
 }
 
-// Validates if the phone number is a valid BIGINT and matches a reasonable length
-// We could create a Validation Helper, but for the use it has, here is fine
-const validatePhoneNumber = (phoneNumber) => {
-    // Check if phoneNumber is a valid number and within the BIGINT range
-    if (!/^\d+$/.test(phoneNumber)) {
-        return { isValid: false, message: 'El número de teléfono debe ser un valor numérico.' };
-    }
-
-    // Ensure the phone number fits within the range of a BIGINT (signed 64-bit integer)
-    const min = -9223372036854775808;
-    const max = 9223372036854775807;
-
-    if (phoneNumber < min || phoneNumber > max) {
-        return { isValid: false, message: 'El número de teléfono está fuera del rango de un BIGINT.' };
-    }
-
-    return { isValid: true };
-};
-
 module.exports = { getAsignaturas, createAsignatura };
